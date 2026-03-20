92e9c1938df02f7c76f8e93ab91397a4cd3e4292
Harish Malakar <harishmalakar19@gmail.com>
2026-03-20 18:09:30 +0530

    by bhiman

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("its a end to end github actions setup");
+    res.send("its a end to end github actions setup by bhiman");
 });
 
 const port = Number(process.env.PORT) || 3000;
