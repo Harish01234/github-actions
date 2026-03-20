430fc1952f6c7643f8668bf5b7ed5427192562c1
Harish Malakar <harishmalakar19@gmail.com>
2026-03-20 13:11:53 +0530

    github-actions added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!  v1");
+    res.send("Hello World!  v1 updated");
 });
 
 const port = Number(process.env.PORT) || 3000;
