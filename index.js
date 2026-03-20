64ebeda7a396e9fac5e4b269fb9476058d3abab6
Harish Malakar <harishmalakar19@gmail.com>
2026-03-20 12:59:36 +0530

    all files added

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!  v1");
+});
+
+const port = Number(process.env.PORT) || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
