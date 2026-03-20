84f2d73e07c35e497fdc6280ed1e5f1cd85b4ecf
Harish Malakar <harishmalakar19@gmail.com>
2026-03-20 13:14:59 +0530

    v2 added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!  v1 updated");
+    res.send("Hello World!  v2");
 });
 
 const port = Number(process.env.PORT) || 3000;
